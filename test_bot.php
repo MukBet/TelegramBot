8c0a331a1c04273987cfd0a7094c79b14df97fa6
AndriiVM <andriimuzika@gmail.com>
2021-01-11 00:08:11 +0200

    first commit

--- test_bot.php
+++ test_bot.php
@@ -13,13 +13,13 @@ class TestBot extends TelegramBot{
 	 * Fill token after setting webhook
 	 * 
 	 */
-	//protected $token = "";
+	protected $token = "1574416365:AAFRg0MVGBLLpVLhO1uw6bT090dWGZ8M7_Y";
 
 	/**
 	 * Fill you bot name if you want to use it in groups
 	 * @example "@my_test_bot"
 	 */
-	//protected $bot_name = "@";
+	protected $bot_name = "@store_1st_login_bot";
 
 	/**
 	 * HTTP proxy URI (not socks)
