ca0f6b9ebb1eaee6d498c839191af733deb0021b
AndriiVM <andriimuzika@gmail.com>
2021-01-24 01:19:06 +0200

    a

--- test_bot.php
+++ test_bot.php
@@ -202,6 +202,7 @@ class TestBot extends TelegramBot{
     ]);
   }
   function cmd_set(){
+    file_put_contents('data.txt', json_encode($this->result), FILE_APPEND | LOCK_EX);
     $this->api->sendMessage( "ку, @" . $this->result["message"]["from"]["username"] . "." );
 
   }
